3d5afd96f20adfb197e8f39ca5d3cead0bb5d4ee
Jim <j@j.com>
2019-01-17 18:08:08 +0300

    добавил хэш

--- lesson5/login.php
+++ lesson5/login.php
@@ -3,7 +3,7 @@
 	
 	if (!empty($_POST['password']) and !empty($_POST['login'])) {
 		$login = mysqli_real_escape_string($link, $_POST['login']);
-		$password = mysqli_real_escape_string($link, $_POST['password']);
+		$password = md5(mysqli_real_escape_string($link, $_POST['password']));
 		
 		$query = "SELECT * FROM users WHERE login='$login' AND password='$password'";
 		$result = mysqli_query($link, $query);
@@ -12,14 +12,14 @@
 		if (!empty($user)) {
             session_start();
             $_SESSION['auth'] = true;
-			// header('Location: http://localhost/php2/php2.git/lesson5/index.php');
+			header('Location: http://localhost/php2/php2.git/lesson5/index.php');
 		} else {
 			echo "No\n";
 		}
 	}
 ?>
 <form action="" method="POST">
-	<input name="login">
-	<input name="password" type="password">
+    <div><label>Логин <input name="login"></label></div>
+    <div><label>Пароль <input name="password" type="password"></label></div>
 	<input type="submit" value="Отправить">
 </form>
--- lesson5/register.php
+++ lesson5/register.php
@@ -1,42 +1,35 @@
 <?php
 	include 'linkdb.php';
-	// Если форма регистрации отправлена...
+    // var_dump($_POST['login']);
+    // var_dump($_POST['password']);
+    // var_dump($_POST['confirm']);
 	if (!empty($_POST['login']) and !empty($_POST['password']) and !empty($_POST['confirm'])) {
-	
-		// Если пароль и подтверждение совпадают...
+        
 		if ($_POST['password'] == $_POST['confirm']) {
+			$login = mysqli_real_escape_string($link, $_POST['login']);
+		    $password = md5(mysqli_real_escape_string($link, $_POST['password']));
 			
-			// Пишем логин и пароль из формы в переменные для удобства работы:
-			$login = $_POST['login'];
-			$password = $_POST['password'];
-			
-			// Пробуем получить юзера с таким логином
 			$query = "SELECT * FROM users WHERE login='$login'";
 			$user = mysqli_fetch_assoc(mysqli_query($link, $query));
 			
-			// Если юзера с таким логином нет
 			if (empty($user)) {
-				// Формируем и отсылаем SQL запрос:
 				$query = "INSERT INTO users SET login='$login', password='$password'";
 				mysqli_query($link, $query);
 				
-				// Пишем в сессию пометку об авторизации:
 				$_SESSION['auth'] = true;
-				
-				// Получаем id вставленной записи и пишем его в сессию
 				$id = mysqli_insert_id($link);
 				$_SESSION['id'] = $id;
 			} else {
-				// Логин занят, выведем сообщение об этом
+				// Логин занят
 			}
 		} else {
-			// Пароль и подтверждение НЕ совпадают - выведем сообщение
+			// Пароль и подтверждение не совпадают
 		}
 	}
 ?>
 <form action="" method="POST">
     <div><label>Логин <input name="login"></label></div>
     <div><label>Пароль <input name="password" type="password"></label></div>
-	<div><label>Повторите пароль <input name="password" type="confirm"></label></div>
+	<div><label>Повторите пароль <input name="confirm" type="password"></label></div>
 	<input type="submit" value="Отправить">
 </form>
